aad0127e76a3f15dbb2c0a9094f071ac927e3ca0
Nitin Sharma <bloggernitscomp54engg@gmail.com>
2016-03-21 22:33:05 +0530

    Create sum.cpp

---
+++ cpp/sum.cpp
@@ -0,0 +1,28 @@
+/*
+ *  main.cpp
+ *
+ *  Created on: Mar 20, 2016
+ *  Author: nits-54
+ */
+#include<iostream>
+using namespace std;
+int main(){
+
+	int num1=0;
+	int num2=0;
+
+	cout<<"==========="<<" Sum of two number "<<"==========="<<endl;
+
+	cout<<"Enter the first number : ";
+	cin>>num1;
+	cout<<"Enter the second number : ";
+	cin>>num2;
+	cout<<"Total sum : "<<num1+num2;
+
+  return 1;
+}
+
+
+
+
+
